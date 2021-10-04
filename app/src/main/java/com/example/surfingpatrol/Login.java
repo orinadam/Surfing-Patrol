362d19eeb82260988218b52a19b2e9a07e0107c5
Comps <Comps@Bialik.hsl>
2021-10-04 10:12:35 +0300

    Login updated

--- app/src/main/java/com/example/surfingpatrol/Login.java
+++ app/src/main/java/com/example/surfingpatrol/Login.java
@@ -2,14 +2,36 @@ package com.example.surfingpatrol;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    TextInputLayout loginEmail, loginregPassword;
+    Button loginButton;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        loginEmail = findViewById(R.id.login_email);
+        loginregPassword = findViewById(R.id.login_password);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+            }
+        });
+
     }
 }
